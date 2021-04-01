6eed4d2b062248d4a7745b8c6b50ea658cbd8254
hfbuitrago10 <hf.buitrago10@uniandes.edu.co>
2021-03-31 23:42:10 -0500

    cambios en Controller

--- App/controller.py
+++ App/controller.py
@@ -31,8 +31,61 @@ El controlador se encarga de mediar entre la vista y el modelo.
 
 # Inicialización del Catálogo de libros
 
+def initCatalog():
+    """
+    Llama la función de inicialización del catálogo al modelo
+    """
+    catalog = model.newCatalog()
+    return catalog
+    
 # Funciones para la carga de datos
+
+def loadData(catalog):
+    """
+    Carga los datos de los archivos y carga los datos
+    en las estructuras de datos
+    """
+    loadVideos(catalog)
+    loadCategory(catalog)
+
+def loadVideos(catalog):
+    """
+    Carga los videos del archivo. Por cada video se indica al
+    modelo que debe adicionarlo al catálogo
+    """
+    videosfile = cf.data_dir + 'videos/videos-small.csv'
+    input_file = csv.DictReader(open(videosfile, encoding='utf-8'))
+    for video in input_file:
+        model.addVideo(catalog, video)
+
+def loadCategory(catalog):
+    """
+    Cargar las categorías del archivo. Por cada categoría se indica al
+    modelo que debe adicionarla al catálogo
+    """
+    categoryidfile = cf.data_dir + 'videos/category-id.csv'
+    input_file = csv.DictReader(open(categoryidfile, encoding='utf-8'), delimiter='\t')
+    for category in input_file:
+        model.addCategory(catalog, category)
 
 # Funciones de ordenamiento
 
 # Funciones de consulta sobre el catálogo
+
+def videosSize(catalog):
+    """
+    Número de videos cargados en el catálogo
+    """
+    return model.videosSize(catalog)
+
+def categorySize(catalog):
+    """
+    Número de categorías cargadas en el catálogo
+    """
+    return model.categorySize(catalog)
+
+def getVideosByCountry(catalog, country):
+    """
+    Retorna los videos de un país específico
+    """
+    return model.getVideosByCountry(catalog, country)
