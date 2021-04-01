1c1c7f8105a63235b76217b92608f1057032cba3
hfbuitrago10 <hf.buitrago10@uniandes.edu.co>
2021-03-31 23:41:56 -0500

    cambios en Model

--- App/model.py
+++ App/model.py
@@ -39,12 +39,237 @@ los mismos.
 
 # Construccion de modelos
 
-# Funciones para agregar informacion al catalogo
+def newCatalog():
+    """
+    Inicializa el catálogo de videos. Crea una lista para guardar
+    los videos. Se crean indices (maps) por los siguientes
+    criterios:
+    Video ids
+    Category
+    Category ids
+    """
+    catalog = {'videos': None, 
+               'videoids': None,
+               'category': None, 
+               'categoryids': None,
+               'country': None}
+    
+    """
+    Esta lista contiene los videos del archivo. Los videos son
+    referenciados por los indices creados a continuación
+    """
+    catalog['videos'] = lt.newList('SINGLE_LINKED', compareVideosids)
+
+    """
+    Se crean indices (maps) por diferentes criterios para llegar
+    a la información consultada
+    """
+
+    """
+    Este indice crea un map cuya llave es el video id del video
+    """
+    catalog['videoids'] = mp.newMap(1000,
+                                    maptype='CHAINING',
+                                    loadfactor=4.0,
+                                    comparefunction=compareMapVideosids)
+
+    """
+    Este indice crea un map cuya llave es el category name del video
+    """
+    catalog['category'] = mp.newMap(100, 
+                                      maptype='CHAINING', 
+                                      loadfactor=4.0,
+                                      comparefunction=compareCategory)
+    
+    """
+    Este indice crea un map cuya llave es el category id del video
+    """
+    catalog['categoryids'] = mp.newMap(100,
+                                        maptype='CHAINING',
+                                        loadfactor=4.0,
+                                        comparefunction=compareCategoryids)
+    
+    """
+    Este indice crea un map cuya llave es el país del video
+    """
+    catalog['country'] = mp.newMap(100,
+                                    maptype='CHAINING',
+                                    loadfactor=4.0,
+                                    comparefunction=compareMapCountry)
+    
+    return catalog
+
+# Funciones para agregar informacion al catálogo
+
+def addVideo(catalog, video):
+    """
+    Adiciona un video a la lista de videos, adicionalmente lo guarda
+    en un map usando su id como llave.
+    Además, crea una entrada en el map de paises para indicar que ese
+    video pertenece a un país específico.
+    """
+    lt.addLast(catalog['videos'], video)
+    mp.put(catalog['videoids'], video['video_id'], video)
+    addVideoCountry(catalog, video)
+
+def addCategory(catalog, category):
+    """
+    """
+    newCategory = newVideoCategory(category['name'], category['id'])
+    mp.put(catalog['category'], category['name'], newCategory)
+    mp.put(catalog['categoryids'], category['id'], newCategory)
+
+def addVideoCountry(catalog, video):
+    """
+    Adiciona un video a la lista de videos de un país específico, los
+    paises se guardan en un map, donde la llave es el país y el valor
+    es la lista de videos de ese país
+    """
+    try:
+        countries = catalog['country']
+        videoCountry = video['country']
+        existcountry = mp.contains(countries, videoCountry)
+        if existcountry:
+            entry = mp.get(countries, videoCountry)
+            country = me.getValue(entry)
+        else:
+            country = newVideoCountry(videoCountry)
+            mp.put(countries, videoCountry, country)
+        lt.addLast(country['videos'], video)
+    except Exception:
+        return None
 
 # Funciones para creacion de datos
 
+def newVideoCategory(name, id):
+    """
+    Esta función crea la estructura de videps asociados a
+    una categoría específica
+    """
+    category = {'name': '', 
+                'id': '', 
+                'total_videos': 0,
+                'videos': None}
+    
+    category['name'] = name
+    category['id'] = id
+    category['videos'] = lt.newList()
+    return category
+
+def newVideoCountry(country):
+    """
+    Esta función crea la estructura de videos asociados a
+    un país específico
+    """
+    countryentry = {'country': '',
+                    'videos': None}
+
+    countryentry['country'] = country
+    countryentry['videos'] = lt.newList()
+    return countryentry
+
 # Funciones de consulta
+
+def videosSize(catalog):
+    """
+    Número de videos cargados en el catálogo
+    """
+    return lt.size(catalog['videos'])
+
+def categorySize(catalog):
+    """
+    Número de categorías cargadas en el catálogo
+    """
+    return mp.size(catalog['category'])
+
+def getVideosByCountry(catalog, country):
+    """
+    Retorna los videos de un país específico
+    """
+    country = mp.get(catalog['country'], country)
+    if country:
+        return me.getValue(country)['videos']
 
 # Funciones utilizadas para comparar elementos dentro de una lista
 
+def compareVideosids(id1, id2):
+    """
+    Compara los ids de dos videos
+    """
+    if (id1 == id2):
+        return 0
+    elif id1 > id2:
+        return 1
+    else:
+        return -1
+
+def compareMapVideosids(id, entry):
+    """
+    Compara los ids de dos videos, id es un identificador
+    del video y entry una pareja llave-valor
+    """
+    identry = me.getKey(entry)
+    if (str(id) == str(identry)):
+        return 0
+    elif (str(id) > str(identry)):
+        return 1
+    else:
+        return -1
+
+def compareCategory(name, category):
+    """
+    Compara los category name de dos videos, name es el 
+    nombre de la categoría y category una pareja llave-valor
+    """
+    categoryentry = me.getKey(category)
+    if (name == categoryentry):
+        return 0
+    elif (name > categoryentry):
+        return 1
+    else:
+        return -1
+
+def compareCategoryids(id, category):
+    """
+    Compara los category ids de dos videos, id es un identificador
+    de la categoría y category una pareja llave-valor
+    """
+    categoryentry = me.getKey(category)
+    if (int(id) == int(categoryentry)):
+        return 0
+    elif (int(id) > int(categoryentry)):
+        return 1
+    else:
+        return -1
+
+def compareMapCountry(country, entry):
+    """
+    Compara el país de dos videos, country es el país del
+    video y entry una pareja llave-valor
+    """
+    countryentry = me.getKey(entry)
+    if (country == countryentry):
+        return 0
+    elif (country > countryentry):
+        return 1
+    else:
+        return -1
+
+def compareCountry(country1, country2):
+    """
+    Compara el país de dos videos
+    """
+    if (country1 == country2):
+        return 0
+    elif (country1 > country2):
+        return 1
+    else:
+        return -1
+
 # Funciones de ordenamiento
+
+def sortVideosByViews(catalog):
+    """
+    Ordena el catálogo de videos por sus 'views'
+    """
+    pass
